94852bd76565ba668aa632c68c782d9f0daf5fac
Elena Dimitrovska <dimitrovska.elena1@outlook.com>
2021-06-17 11:27:12 +0200

    added users

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
 import logo from './logo.svg';
 import './App.css';
+import { Navbar, Nav } from 'react-bootstrap'
+import Home from './Home';
+import About from './About';
+import Users from './Users';
+
+import { Link, Route, BrowserRouter as Router, Switch } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Navbar bg="primary" variant="dark">
+          <Navbar.Brand>Navbar</Navbar.Brand>
+          <Nav className="mr-auto">
+            <Nav.Link><Link to="/">Home</Link></Nav.Link>
+            <Nav.Link><Link to="/about">About</Link></Nav.Link>
+            <Nav.Link><Link to="/users">Users</Link></Nav.Link>
+          </Nav>
+        </Navbar>
+
+        <Switch>
+          <Route path="/about" component={About} />
+          <Route path="/users" component={Users} />
+          <Route path="/" component={Home} />
+        </Switch>
+      </Router>
     </div>
   );
 }
